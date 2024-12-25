99468c924da802787a3bb307479954df409608c5
ndikzone <baksocloud@gmail.com>
2024-12-25 16:56:26 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -1,18 +1,18 @@
 /**let moment = require('moment-timezone')
 let handler = m => m
 
-*//handler.before = async function (m) {
+handler.before = async function (m) {
 
     if (m.chat.endsWith('broadcast')) return
     if (m.fromMe) return
-    if (m.isGroup) return
-    if (db.data.settings.groupOnly) return
-  let user = global.db.data.users[m.sender]
+    if (m.isGroup) return**/
+   // if (db.data.settings.groupOnly) return
+/**  let user = global.db.data.users[m.sender]
     let { banned } = db.data.users[m.chat]
     let username = conn.getName(m.sender) 
-   if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
-    await conn.modifyChat(m.chat, 'mute', -Math.floor(new Date / 1e3) * 1e3 - 1e3).catch(console.log)
-   await this.sendButton(m.chat, `
+   if (new Date - user.pc < 86400000) return**/ // setiap 24 jam sekali
+ //   await conn.modifyChat(m.chat, 'mute', -Math.floor(new Date / 1e3) * 1e3 - 1e3).catch(console.log)
+/**   await this.sendButton(m.chat, `
 Hai ${ucapan()}
 
 ${banned ? `kamu *terbanned* kak 😕\nHubungi: wa.me/${owner[0]}` : 'Ada Yang Bisa Saya Bantu?'}
@@ -20,8 +20,8 @@ ${banned ? `kamu *terbanned* kak 😕\nHubungi: wa.me/${owner[0]}` : 'Ada Yang B
     user.pc = new Date * 1
 }
 
-/**module.exports = handler
-*//function ucapan() {
+module.exports = handler
+function ucapan() {
         const hour_now = moment.tz('Asia/Jakarta').format('HH')
         var ucapanWaktu = 'Pagi kak'
         if (hour_now >= '03' && hour_now <= '10') {
@@ -38,4 +38,4 @@ ${banned ? `kamu *terbanned* kak 😕\nHubungi: wa.me/${owner[0]}` : 'Ada Yang B
           ucapanWaktu = 'Selamat Malam!'
         }	
         return ucapanWaktu
-}
+}**/
