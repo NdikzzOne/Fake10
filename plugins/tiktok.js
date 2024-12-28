095f5718067e4162bbc15164f67f8a861707e7ca
NdikzzOne <dikacanda405@gmail.com>
2024-12-28 11:29:02 +0700

    Add files via upload

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,22 +1,12 @@
-const axios = require('axios')
-const fetch = require('node-fetch')
-const cheerio = require('cheerio')
-const got = require('got')
-
+let fetch = require('node-fetch')
+let fs = require('fs')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args || !args[0]) throw `✳️ Contoh :\n${usedPrefix + command} https://www.tiktok.com/xxxxx`
     if (!(args[0].includes('http://') || args[0].includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
     if (!args[0].includes('tiktok.com')) return m.reply(`Invalid Tiktok URL.`)
-    await conn.sendMessage(m.chat, {
-            react: {
-                text: "⚡",
-                key: m.key,
-            }
-        })
-    let text = args[0]
-    let spas = "                "
-    try {
-        let nt = await fetch(`https://api.tiklydown.eu.org/api/download?url=${args[0]}`)
+ //   if (!args[0]) throw `*• Example:* ${usedPrefix + command} https://youtu.be/xxxx`
+    m.reply(wait)
+let nt = await fetch(`https://api.tiklydown.eu.org/api/download?url=${args[0]}`)
         let vas = await nt.json()
   let v = vas.video
   let s = vas.author
@@ -35,54 +25,8 @@ ${wm}`
                 key: m.key,
             }
         })
-    } catch (e) {
-        console.error(e)
-        await conn.sendMessage(m.chat, {
-            react: {
-                text: "⏳",
-                key: m.key,
-            }
-        })
-        m.reply(`––––––『 ⎔ 𝙇𝙤𝙖𝙙𝙞𝙣𝙜... 』––––––\n\nJika Eror Bisa ketik: .tt2`);
-        try {
-                            let ler = await axios.get(`${webapi}api/downloader/tiktok?url=${text}&apikey=${apichan}`)
-            let cer = ler.data.data
-			console.log(cer)
-            let cap = `${spas}*「 T I K T O K 」*
-
-*📛Author:* ${cer.nickname}
-*📒Title:* ${cer.description}
-
-KETIK .ttmp3 UNTUK MENDOWNLOAD AUDIO
-${wm}`
-            conn.sendFile(m.chat, cer.play, 'mp.4', cap, m)
-            conn.sendMessage(m.chat, {
-                react: {
-                    text: "✅",
-                    key: m.key,
-                }
-            })
-        } catch (e) {
-            console.error(e)
-            conn.sendMessage(m.chat, {
-                react: {
-                    text: "✅",
-                    key: m.key,
-                }
-            })
-            let data = await axios.get(`https://aemt.me/download/ttdl?url=${text}`);
-            console.log(data.data);
-            let capt = `${spas}*「 T I K T O K 」*
-
-*📛Author:* ${wm}
-*📒Title:* ${data.data.result.title}
-
-KETIK .ttmp3 UNTUK MENDOWNLOAD AUDIO
-${wm}`;
-            conn.sendFile(m.chat, data.data.result.video[0], 'mp4', capt, m);
-        }
     }
-}
+//conn.sendFile(m.chat, hasil, '', wm, m)
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.limit = 5
