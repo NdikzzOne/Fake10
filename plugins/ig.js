0cc8f4d1e5c0b9211f6af9f0c665f531ebbe9e0b
NdikzzOne <dikacanda405@gmail.com>
2024-12-29 16:11:36 +0700

    Add files via upload

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,29 +1,32 @@
+var { fg } = require('api-dylux')
 let fetch = require('node-fetch')
-let fs = require('fs')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+var { youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
+let limit = 350 
+let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, command }) => {
     if (!args || !args[0]) throw `✳️ Contoh :\n${usedPrefix + command} https://www.instagram.com/xxxxx`
-    if (!(args[0].includes('http://') || args[0].includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
-    if (!args[0].includes('instagram.com')) return m.reply(`Invalid instagram URL.`)
- //   if (!args[0]) throw `*• Example:* ${usedPrefix + command} https://youtu.be/xxxx`
-    m.reply(wait)
-let nt = await fetch(`${neNdikz}api/ig?url=${args[0]}&apikey=${neoapi}`)
-        let vas = await nt.json()
-  let v = vas.data
-        console.log(vas.url[0])
-        conn.sendFile(m.chat, v.url, null, wm, m)
-        conn.sendMessage(m.chat, {
-            react: {
-                text: "✅",
-                key: m.key,
-            }
-        })
+    if (!(args[0].includes('http://') || text.includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
+    if (!args[0].includes('instagram.com')) return m.reply(`Invalid Tiktok URL.`)
+	//if (!args || !args[0]) throw `✳️ Contoh :\n${usedPrefix + command} https://www.instagram.com/xxxxx`
+	 let chat = global.db.data.chats[m.chat]
+	 m.reply(wait) 
+    try {
+			let res = await fetch(`${alyaNdikz}api/ig?url=${args[0]}&apikey=${alyaapi}`)
+  let vas = await res.json()
+  let vvas = vas.data[0]
+    
+    conn.sendFile(m.chat, vvas.url, null, wm, m)
+    } catch (e) {
+        let res = await fetch(`${webapi}api/downloader/instagram2?url=${args[0]}&apikey=${apichan}`)
+  let vas = await res.json()
+  let vvas = vas.data[0]
+    
+    conn.sendFile(m.chat, vvas.url, null, wm, m)
     }
-//conn.sendFile(m.chat, hasil, '', wm, m)
-handler.help = ['ig'].map(v => v + ' <url>')
+    }
+
+handler.help = ['ig']
 handler.tags = ['downloader']
 handler.limit = 5
-handler.command = /^(ig|instagram)$/i
-
-handler.limit = true
+handler.command = /^(Instagram|ig|igdl)$/i
 
 module.exports = handler
